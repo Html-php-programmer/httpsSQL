4a1bbb1dfbd381460ed56901c0335b9181f6db11
Vlad satonin <vlad.satonin74@gmail.com>
2019-12-21 11:29:49 +0500

    спринт 1

--- Javas.js
+++ Javas.js
@@ -26,18 +26,61 @@ function getResponceText(xmlHttp) {
 }
 
 function Display(obj) {
+  var table = document.getElementById('tab');
+  var table = document.querySelector('table');
+  table.parentNode.removeChild(table);
 
+  var table1 = document.createElement('table');
+  table1.setAttribute("id", "fio");
+  var thead = document.createElement('thead');
+  var tr = document.createElement('tr');
+  var th = document.createElement('th');
+  var surname = document.createTextNode('Фамилия');
+  th.appendChild(surname);
+  tr.appendChild(th);
+  thead.appendChild(tr);
+  table1.appendChild(thead);
+  var tbody = document.createElement('tbody');
+  for (var i = 0; i < obj.length; i++) {
+    var tr = document.createElement('tr');
+    var td = document.createElement('td');
+    var sn = document.createTextNode(obj[i]);
+    td.appendChild(sn);
+    tr.appendChild(td);
+    tbody.appendChild(tr);
+  }
+  table1.appendChild(tbody);
+  var oper = document.getElementById('operac');
+  var parentDiv = oper.parentNode;
+  parentDiv.insertBefore(table1, oper);
 }
 
 function Receive(req) {
-
+  req.onreadystatechange = () => {
+    if (req.readyState == 4) {
+      if (req.status == 200) {
+        let obj = JSON.parse(req.responseText);
+        Display(obj);
+      } else {
+        // alert("error");
+        console.log("error");
+      }
+    } else {
+      // alert("error");
+      console.log("error");
+    }
+  }
 }
 function Init(value) {
   // создание объекта для выполнения AJAX-запроса
   // и назначение обработчика ответа
   var req = createXmlHttpRequestObject();
+  req.open('POST', 'ini.php');
+  req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  Send(req, value);
+  Receive(req);
 }
 
 function Send(req, value) {
-
+  req.send("gruppa=" + value);
 }
